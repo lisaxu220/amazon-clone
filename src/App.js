5f25d5e40592ddfcdeb5de8f620e73866d56ad99
Lisa Xu <lisaxu220@hotmail.com>
2021-10-06 13:32:48 +0400

    Create header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import './App.css';
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, world!</h1>
+    <div className="app">
+      <Header></Header>
+      {/* Home */}
     </div>
   );
 }
