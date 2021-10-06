13e18c9ece9e09f63e9e14b66c4ac22bc7f01096
Lisa Xu <lisaxu220@hotmail.com>
2021-10-06 12:44:55 +0400

    Setup React app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBU5kBqeAp29B5xla8uMSORNu0MA5CDsYE",
+  authDomain: "clone-48a97.firebaseapp.com",
+  projectId: "clone-48a97",
+  storageBucket: "clone-48a97.appspot.com",
+  messagingSenderId: "577376171574",
+  appId: "1:577376171574:web:90cfb0941b1d0f938d4a97"
+};
