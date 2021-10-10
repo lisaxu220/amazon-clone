eb72724e0c316e367b740da8fe42f47ef3ba89c4
Lisa Xu <lisaxu220@hotmail.com>
2021-10-10 16:56:14 +0400

    Deploy function to FIrebase

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/clone-48a97/us-central1/api'
+  baseURL: 'https://us-central1-clone-48a97.cloudfunctions.net/api'
+  // 'http://127.0.0.1:5001/clone-48a97/us-central1/api'
 });
 
 export default instance;
